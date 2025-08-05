a5ceeec5fc805b6152cd4e4464ba49c4b73e645b
Zaw Tun Naing <tunnaingzaw@comfortdelgrobus.com.sg>
2025-08-05 21:31:39 +1200

    Add Ntire Architecture Default Class Library

---
+++ MaxStore.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using MaxStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MaxStore.Data
+{
+    public class ApplicationDbContext : DbContext   
+    {
+        public ApplicationDbContext( DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "မုန့်ဟင်းခါး၊ အုန်းနို့ခေါက်ဆွဲ၊ ရခိုင်မုန့်တီ နဲ့ အသုပ်အမျိုးမျိုး",  DisplayOrder = 1 },
+                new Category { Id = 2, Name = "ရှမ်းအစားအသောက်များ", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "ကော်ဖီ၊ လက်ဖက်ရည် နဲ့ လက်ဖက်ခြောက်",  DisplayOrder = 3 },
+                new Category { Id = 4, Name = "အသင့်စားစည်သွပ်ဘူးများ",  DisplayOrder = 4 },
+                new Category { Id = 5, Name = "အရံဟင်းလျာများ", DisplayOrder = 5 },
+                new Category { Id = 6, Name = "လက်ဖက် နဲ့ အကြော်စုံ",  DisplayOrder = 6 },
+                  new Category { Id = 7, Name = "ငါးပိ၊ ငါးခြောက် နဲ့ ပုဇွန်ခြောက်",  DisplayOrder = 7 },
+                   new Category { Id = 8, Name = "Medicine", DisplayOrder = 8 }
+
+                );
+        }
+    }
+}
